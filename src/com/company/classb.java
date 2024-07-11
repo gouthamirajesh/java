a056716f855e882bdb834766962d67cb5d0ebcce
rajesh.malempati94@gmail.com <gouthamigorantla@gmail.com>
2024-07-10 21:28:55 -0500

    test commit

---
+++ src/com/company/classb.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class classb
+{
+
+    public void meth2(){
+
+        System.out.println("class b called");
+    }
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
