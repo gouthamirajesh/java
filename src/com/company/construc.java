79e5fb911cb49702aecfb968813e34e15b621b40
rajesh.malempati94@gmail.com <rajesh.malempati94@gmail.com>
2024-07-10 21:47:39 -0500

    test commit

---
+++ src/com/company/construc.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class construc
+{
+
+    int x;
+    construc()
+    {
+        System.out.println("1st non-peramet-- construcor");
+    }
+    construc(int a, int b)
+    {
+        System.out.println((a+b));
+    }
+    construc(int c, String d)
+    {
+
+    }
+}
